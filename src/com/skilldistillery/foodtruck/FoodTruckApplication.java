1d6307b80e88b3813387f59be8d92d2c24324919
therandomfool <mddenver@protonmail.com>
2020-03-28 12:12:01 -0700

    Basic skeleton of FoodTruckApp/Generator

---
+++ src/com/skilldistillery/foodtruck/FoodTruckApplication.java
@@ -0,0 +1,24 @@
+package com.skilldistillery.foodtruck;
+
+import java.util.Scanner;
+
+public class FoodTruckApplication {
+
+	public static void main(String[] args) {
+		int MAX_FT =5;
+		FoodTruckApplication brandSpankingNewTruck = new FoodTruckApplication();
+		Scanner input = new Scanner(System.in);
+		
+
+	}
+
+//	FoodTruckApplication brandSpankingNewTruck = new FoodTruckApplication();
+	
+	void MakeMeAFoodTruckBaby(){
+		
+		
+		
+		
+		
+	}
+}
