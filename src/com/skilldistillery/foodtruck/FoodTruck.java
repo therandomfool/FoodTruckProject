81476092b5f6bd32ae48186a4750823816ea550d
therandomfool <mddenver@protonmail.com>
2020-03-28 14:30:18 -0700

    Added Static method, FoodTruck Creation

---
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -0,0 +1,60 @@
+package com.skilldistillery.foodtruck;
+
+public class FoodTruck {
+	private String nomDeGuerre;
+	private String nomNomNom;
+	private Double showMeYourStars;
+	private int youAreUnique = 0;
+	private static int iAmANumber = 1;
+
+	public FoodTruck() {
+		super();
+		youAreUnique = iAmANumber++;
+
+	}
+
+
+	public FoodTruck(String nomDeGuerre, String nomNomNom, Double showMeYourStars) {
+		this();
+		this.nomDeGuerre = nomDeGuerre;
+		this.nomNomNom = nomNomNom;
+		this.showMeYourStars = showMeYourStars;
+	}
+
+
+	public String getNomDeGuerre() {
+		return nomDeGuerre;
+	}
+
+
+	public void setNomDeGuerre(String nomDeGuerre) {
+		this.nomDeGuerre = nomDeGuerre;
+	}
+
+
+	public String getNomNomNom() {
+		return nomNomNom;
+	}
+
+
+	public void setNomNomNom(String nomNomNom) {
+		this.nomNomNom = nomNomNom;
+	}
+
+
+	public Double getShowMeYourStars() {
+		return showMeYourStars;
+	}
+
+
+	public void setShowMeYourStars(Double showMeYourStars) {
+		this.showMeYourStars = showMeYourStars;
+	}
+
+
+	@Override
+	public String toString() {
+		return "FoodTruck [nomDeGuerre=" + nomDeGuerre + ", nomNomNom=" + nomNomNom + ", showMeYourStars="
+				+ showMeYourStars + ", youAreUnique=" + youAreUnique + "]";
+	}
+}
--- src/com/skilldistillery/foodtruck/FoodTruckApplication.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApplication.java
@@ -3,22 +3,47 @@ package com.skilldistillery.foodtruck;
 import java.util.Scanner;
 
 public class FoodTruckApplication {
+	Scanner input = new Scanner(System.in);
+	int MAX_FT =5;
+	private FoodTruck daFoodTruck[] = new FoodTruck[5];
 
-	public static void main(String[] args) {
-		int MAX_FT =5;
-		FoodTruckApplication brandSpankingNewTruck = new FoodTruckApplication();
-		Scanner input = new Scanner(System.in);
-		
-
+	public static void main(String[] args) {	
+		FoodTruckApplication ftApp = new FoodTruckApplication();
+		ftApp.MakeMeAFoodTruckBaby();
 	}
 
-//	FoodTruckApplication brandSpankingNewTruck = new FoodTruckApplication();
 	
-	void MakeMeAFoodTruckBaby(){
+	public void MakeMeAFoodTruckBaby(){
+	for (int i = 0; i < daFoodTruck.length; i++) {
 		
+		System.out.println("What is the name for your new mobile culinary platform(i.e. Food Truck's name)??? : \t");
+		String nomDeGuerre = input.nextLine();
+		
+		nomDeGuerre = nomDeGuerre.toLowerCase();
+		if (nomDeGuerre.equals("quit")) {
+			System.out.println("* * * * * * * * You have choosen to quit * * * * * * * *");
+			break;
+		}
+		
+		System.out.println("In one word describe your cuisine (i.e. Mexican, Vietmanese, Korean): \t");
+		String nomNomNom = input.nextLine();
+		
+		System.out.println("How much is " + nomDeGuerre + "'s" + " " + nomNomNom + " Food Truck liked??? (i.e. break all those subjective feelings down to a one to five rating)\t" );
+		double showMeYourStars = input.nextDouble();
+		
+		daFoodTruck[i] = new FoodTruck(nomDeGuerre, nomNomNom, showMeYourStars);
+//		System.out.println(daFoodTruck[i]);
+		
+	}
 		
 		
 		
 		
 	}
+
+//	@Override
+//	public String toString() {
+//		return "FoodTruckApplication [input=" + input + ", MAX_FT=" + MAX_FT + ", daFoodTruck="
+//				+ Arrays.toString(daFoodTruck) + "]";
+//	}
 }
